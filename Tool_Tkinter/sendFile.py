7aedd6815b42cfcb2fe30971abafc391becd30ba
Moritz Konrad <moritzkonrad53@gmail.com>
2024-02-11 23:33:16 +0100

    Protokoll implementiert

--- Tool_Tkinter/sendFile.py
+++ Tool_Tkinter/sendFile.py
@@ -67,82 +67,80 @@ class bcolors:
 def fileToTextBitByBit():
            
     #Init Variablen
-    inputfile_name = 'C:\\Users\\morit\\OneDrive\\Studium\\6. Semester\\Studienarbeit 2\\Umsetzung\\VSCode\\Testen\\Testen\\InputFiles\\testFile.xlsx'
-
-    data = ''
-    prefix = ''
-    postfix_1 = '"'
-    postfix_2 = '>'
-
-    ackStart = 'loslegen'
-    ack = 'nextPlease'
-
+    inputfile = 'C:\\Users\\morit\\OneDrive\\Studium\\6. Semester\\Studienarbeit 2\\Umsetzung\\VSCode\\Testen\\Testen\\InputFiles\\testFile.xlsx'
     blockLength = 5120 #5KB
-    nextBlockPos = 0
     #----------------------------------------------------------------------------------------
 
     #Pfad von "inputfile" wird auf Existenz überprüft
-    if not(os.path.exists(inputfile_name)):
+    if not(os.path.exists(inputfile)):
         print(f"{bcolors.WARNING}")
-        print('Warning:  Input file ' + '\''+inputfile_name + '\''+' does not exist!')
+        print('Warning:  Input file ' + '\''+inputfile + '\''+' does not exist!')
         print(f"{bcolors.ENDC}")
         sys.exit(2)
 
     #----------------------------------------------------------------------------------------
 
     #Ermittlung und Prüfung des Dateityps --> HTML-Präfix generieren (DateiEndung/MIMEType)
-    kind = filetype.guess(inputfile_name)
+    kind = filetype.guess(inputfile)
     if kind is None:
         print(f"{bcolors.WARNING}\r\n*** ERROR Invalid filetype ! ***\r\n{bcolors.ENDC}")
         sys.exit(2)     
     else:
         prefix = '<src="data:' + kind.extension + ';charset=utf-8;base64,' #DateiEndung wird angehängt (nicht der MIMEType)
 
+    #Dateiname ermitteln
+    inputfile_name = ''
     #----------------------------------------------------------------------------------------
-    #Prefixbehandlung noch machen!!!
-    #Input Datei in Segment-Dateien aufsplitten
-    with open(inputfile_name, 'rb') as binary_file:
-            segment_number = 1
-            while True:
-                block_data = binary_file.read(blockLength)
-                if not block_data:
-                    break
-                with open(f'{inputfile_name}_segment_{segment_number}', 'wb') as segment_file:
-                    segment_file.write(block_data)
-                segment_number += 1
+        
+    #Protokoll - Vorgang abgestimmt mit der Receive Funktion starten
+    pyperclip.copy('toRe_StartSending')#Verfügbarkeit anfragen
+    while pyperclip.paste() != 'toSe_StartSending':#Warten bis Verfügbarkeit bestätigt
+        time.sleep(0.05)
+    
+    #Prefixbehandlung noch machen
+    
+    
+    segment_number = 1
+    nextBlockPos = 0
+    #Ordner, in dem das Segment kurzzeitig abgelegt wird; für das finale Tool anpassen/ geeignet wählen / evtl. Ordner, der durch das Tool (exe) generiert wird
+    segment_stack = 'C:\\Users\\morit\\OneDrive\\Studium\\6. Semester\\Studienarbeit 2\\Umsetzung\\VSCode\\Testen\\Testen\\InputFiles'
+    #Input Datei segmentweise versenden
+    while True:
 
-    #Segmentdateien B64 kodieren, nacheinander in die Zwischenablage kopieren und wenn nicht mehr benötigt löschen
-    #Dateipfad wo die Segmente abgelegt werden für das finale Tool anpassen/ geeignet wählen
-    inputFolderpath = 'C:\\Users\\morit\\OneDrive\\Studium\\6. Semester\\Studienarbeit 2\\Umsetzung\\VSCode\\Testen\\Testen\\InputFiles'
-    #Dateipfad evtl. auf Existenz und vorhandenen Inhalt überprüfen
-    segmente = os.listdir(inputFolderpath)
-
-    i=1
-    for segmentfile_name in segmente[1:]:#[1:] Anweisung nur wegen aktuellem Dateipfad weil das testFile noch da drin liegt
-        #Aktuelles Segment base64 kodieren
-        with open(inputFolderpath + '\\' + segmentfile_name, 'rb') as binary_file:
+        #Inputdatei öffnen und passendes Segment im Segment Stack Ordner erzeugen 
+        with open(inputfile, 'rb') as binary_file:
+            binary_file.seek(nextBlockPos,0)
+            block_data = binary_file.read(blockLength)
+            #Wenn kein Segment mehr gebildet werden kann -> Versenden beenden
+            if not block_data:
+                #Protokoll - Der Receive Funktion mitteilen, dass es keine Daten mehr zu versenden gibt
+                pyperclip.copy('toRe_Weiter;Beenden')
+                break
+            with open(f'{segment_stack + '\\' + inputfile_name}_segment_{segment_number}', 'wb') as segment_file:
+                segment_file.write(block_data)
+        
+        #Generiertes Segment versenden
+        segmentfile_name = os.listdir(segment_stack)[1]#[1] Anweisung nur wegen aktuellem Dateipfad weil das testFile noch da drin liegt
+        #Segment base64 kodieren
+        with open(segment_stack + '\\' + segmentfile_name, 'rb') as binary_file:
             binary_file_data = binary_file.read()
             base64_encoded_data = base64.b64encode(binary_file_data)
-            data_url=base64_encoded_data.decode('utf-8')
-       
-        #"Preamble" um Vorgang abgestimmt mit Send Funktion zu starten         
-        if i == 1:
-            pyperclip.copy('start')
-            while pyperclip.paste() != ackStart:
-                    time.sleep(0.05)
-        i += 1
-
-        #Entstandene base64 dataURL in die Zwischenablage schreiben und auf ein acknowledgement warten
-        pyperclip.copy(data_url)
+            base64_endcoded_segment= 'toRe_Weiter;'+base64_encoded_data.decode('utf-8')#Protokoll - Header um der Receive Funktion zu sagen, dass ein Segment zum verarbeiten in der Zwischenablage liegt
+        #Base64 kodiertes Segment in die Zwischenablage schreiben
+        pyperclip.copy(base64_endcoded_segment)
         print(f"{bcolors.OKGREEN}\r\n*** B64 block successfully copied to clipboard !!! ***\r\n{bcolors.ENDC}")
-        while pyperclip.paste() != ack:
-                time.sleep(0.05)
 
-        #Löschen des aktuellen Segmentes, wenn nicht mehr benötigt (durch das ack wird dies klar); Evtl. Farben für Error Messages noch machen
+        #Protokoll - Warten, bis die Receive Funktion den Empfang bestätigt hat
+        while pyperclip.paste() != 'toSe_ProceedSending':
+            time.sleep(0.05)
+        
+        #Löschen des Segmentes
         try:
-            os.remove(inputFolderpath + '\\' + segmentfile_name)
-            print(f"Segment file '{inputFolderpath + '\\' + segmentfile_name}' deleted successfully.")
+            os.remove(segment_stack + '\\' + segmentfile_name)
+            print(f"Segment file '{segment_stack + '\\' + segmentfile_name}' deleted successfully.")
         except OSError as e:
-            print(f"Error deleting segment file '{inputFolderpath + '\\' + segmentfile_name}': {e}")
-
-    #postfixbehandlung noch machen!!!  
+            print(f"Error deleting segment file '{segment_stack + '\\' + segmentfile_name}': {e}")
+        
+        #Laufvariablen neu berechnen
+        nextBlockPos = nextBlockPos + blockLength  
+        segment_number += 1
